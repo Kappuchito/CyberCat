2327a529f9cdf42ebede50864aa64b6de23987c4
Ricardo <ricardo_palma_leiva@outlook.com>
2018-11-15 15:41:23 -0300

    ESP32 libreria hcsr04
    se logra prender un led con sensor y libreria

--- Prueba_Git/Prueba_Git.ino
+++ Prueba_Git/Prueba_Git.ino
@@ -1,34 +1,22 @@
 #include <HCSR04.h>
 
-HCSR04 sensor(11,12); // (PIN Trigger, PIN Echo)
-HCSR04 sensor1(8,9); // (PIN Trigger, PIN Echo)
+HCSR04 sensor(5,4); // (PIN Trigger, PIN Echo)
+int led = 18;
 
-int led = 2;
-int led2 = 3;
-int presion = A1;
-int fuerza;
 void setup()
 {
   pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.write("inicio");
 }
 
 
 void loop()
 {
  float distancia1;
- float distancia2;
  distancia1 = sensor.dist();
- distancia2 = sensor1.dist();
- /*
- Serial.print ("la distancia del primer sensor es: ");
- Serial.print(distancia1);
- Serial.print(" y la del segundo es: ");
- Serial.print(distancia2);
- Serial.print(" y la presion es: ");
- Serial.println(fuerza);*/
  delay(500);
+
 
 if(distancia1 > 100)
 {
@@ -42,23 +30,8 @@ else
   Serial.print(distancia1);
   Serial.println("%");
 }
-if(distancia2 > 100)
-{
-  //si es mayor debo mandar un error
-  Serial.println("Error: medicion sobrepasa el 100% en el segundo sensor");
-  Serial.println("*********************************************************");
-  delay(1000);
-}
-else
-{
-  // aca significa que es menor, darle el porcentaje
-  Serial.print("Segundo sensor: ");
-  Serial.print(distancia2);
-  Serial.println("%");
-  Serial.println("*********************************************************");
-  delay(1000);
-}
-/* if (distancia1>20)
+
+ if (distancia1>50)
  {
     digitalWrite(led, HIGH);
  }
@@ -66,12 +39,4 @@ else
  {
     digitalWrite(led, LOW);
  }
- if(distancia2>20)
- {
-    digitalWrite(led2, HIGH);
- }
- else
- {
-    digitalWrite(led2, LOW);
- }*/
 }
